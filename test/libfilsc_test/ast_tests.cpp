307e1a03c4ba14319830b591a895d64850d6923d
GuillermoHernan <guillermo.hernan.martin@gmail.com>
2018-03-12 17:16:11 +0100

    Test for 'AstNode::create' function.

--- test/libfilsc_test/ast_tests.cpp
+++ test/libfilsc_test/ast_tests.cpp
@@ -25,3 +25,25 @@ TEST(AST, to_string_from_string)
 	ASSERT_STREQ("BAD_AST_TYPE", astTypeToString(AST_TYPES_COUNT).c_str());
 	ASSERT_STREQ("BAD_AST_TYPE", astTypeToString((AstNodeTypes)-1).c_str());
 }
+
+/// <summary>
+/// Tests the node creation function.
+/// </summary>
+TEST(AstNode, create)
+{
+	auto node = AstNode::create(
+		AST_FUNCTION,
+		ScriptPosition(1, 2),
+		"test_function",
+		"test_value",
+		ASTF_CONST
+	);
+
+	EXPECT_EQ(AST_FUNCTION, node->getType());
+	EXPECT_EQ(1, node->position().line);
+	EXPECT_EQ(2, node->position().column);
+	EXPECT_STREQ("test_function", node->getName().c_str());
+	EXPECT_STREQ("test_value", node->getValue().c_str());
+	EXPECT_TRUE(node->hasFlag(ASTF_CONST));
+	EXPECT_EQ(ASTF_CONST, node->getFlags());
+}
