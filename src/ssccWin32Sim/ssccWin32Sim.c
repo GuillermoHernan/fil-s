28de5c2e5f53e27667c96d9be41e95349b5f433b
GuillermoHernan <guillermo.hernan.martin@gmail.com>
2018-04-04 12:24:01 +0200

    Stub for Win32 simulator system specific code.

---
+++ src/ssccWin32Sim/ssccWin32Sim.c
@@ -0,0 +1,14 @@
+/// <summary>
+/// System specific 'C' code for Win32 simulator platform.
+/// </summary>
+
+#include <stdio.h>
+
+/// <summary>
+/// Entry point
+/// </summary>
+/// <returns></returns>
+int main()
+{
+    printf("Test, test, test\n");
+}
